5d0217617a46a15c647fcf8e1d0efc21d3662ab3
Phoomss <narongsak5az@gmail.com>
2026-01-31 21:14:30 +0700

    add interceptor

--- cm-backend/src/main.ts
+++ cm-backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/transform/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // response with data
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
