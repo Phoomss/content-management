e7734411cb594abdd8877f73dcf7141a233858a9
Phoomss <narongsak5az@gmail.com>
2026-02-02 16:15:51 +0700

    complated crud post module

--- cm-backend/src/posts/dto/create-post.dto.ts
+++ cm-backend/src/posts/dto/create-post.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsOptional, IsString, IsNumber } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  content?: string;
+
+  @IsNumber()
+  authorId: number;
+}
--- cm-backend/src/posts/posts.service.ts
+++ cm-backend/src/posts/posts.service.ts
@@ -1,26 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './entities/post.entity';
+import { User } from '../users/entities/user.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectRepository(Post)
+    private postRepo: Repository<Post>,
+
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+
+  async create(createPostDto: CreatePostDto) {
+    const author = await this.userRepo.findOne({
+      where: { id: createPostDto.authorId },
+    });
+
+    if (!author) {
+      throw new NotFoundException('Author not found');
+    }
+
+    const post = this.postRepo.create({
+      title: createPostDto.title,
+      content: createPostDto.content,
+      author,
+    });
+
+    return await this.postRepo.save(post);
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  async findAll() {
+    return await this.postRepo.find({
+      relations: ['author', 'comments'],
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number) {
+    const post = await this.postRepo.findOne({
+      where: { id },
+      relations: ['author', 'comments'],
+    });
+
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    return post;
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    const post = await this.findOne(id);
+
+    Object.assign(post, updatePostDto);
+
+    return await this.postRepo.save(post);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: number) {
+    const post = await this.findOne(id);
+    await this.postRepo.remove(post);
+
+    return { deleted: true };
   }
 }
