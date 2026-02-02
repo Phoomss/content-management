fee5a18e523e39e97b4ac84ed160a91333f6a854
Phoomss <narongsak5az@gmail.com>
2026-02-02 17:52:09 +0700

    make structure authen and init depen about authen

--- cm-backend/src/app.module.ts
+++ cm-backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -28,6 +29,7 @@ import { ConfigModule } from '@nestjs/config';
     UsersModule,
     PostsModule,
     CommentsModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
