50de33e1131f8f09f0ac104ab720403587d16561
Phoomss <narongsak5az@gmail.com>
2026-02-02 16:40:52 +0700

    complate commnet api

--- cm-backend/src/comments/comments.service.ts
+++ cm-backend/src/comments/comments.service.ts
@@ -1,26 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { Repository } from 'typeorm';
+import { Post } from '../posts/entities/post.entity';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(
+    @InjectRepository(Comment)
+    private commentRepo: Repository<Comment>,
+
+    @InjectRepository(Post)
+    private postRepo: Repository<Post>,
+
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+
+  async create(createCommentDto: CreateCommentDto) {
+    const post = await this.postRepo.findOne({
+      where: { id: createCommentDto.postId },
+    });
+
+    if (!post) throw new NotFoundException('Post not found');
+
+    const user = await this.userRepo.findOne({
+      where: { id: createCommentDto.userId },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    const comment = this.commentRepo.create({
+      message: createCommentDto.message,
+      post,
+      user,
+    });
+
+    return await this.commentRepo.save(comment);
   }
 
-  findAll() {
-    return `This action returns all comments`;
+  async findAll() {
+    return await this.commentRepo.find({
+      relations: ['user', 'post'],
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  async findOne(id: number) {
+    const comment = await this.commentRepo.findOne({
+      where: { id },
+      relations: ['user', 'comment'],
+    });
+
+    if (!comment) throw new NotFoundException('comment not found');
+
+    return comment;
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+  async update(id: number, updateCommentDto: UpdateCommentDto) {
+    const comment = await this.commentRepo.findOne({
+      where: { id },
+    });
+
+    if (!comment) throw new NotFoundException('Comment not found');
+
+    Object.assign(comment, updateCommentDto);
+
+    return await this.commentRepo.save(comment);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  async remove(id: number) {
+    const comment = await this.commentRepo.findOne({
+      where: { id },
+    });
+
+    if (!comment) throw new NotFoundException('Comment not found');
+
+    await this.commentRepo.remove(comment);
+
+    return { deleted: true };
   }
 }
--- cm-backend/src/comments/dto/create-comment.dto.ts
+++ cm-backend/src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCommentDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsString()
+  @IsNotEmpty()
+  message: string;
+
+  @IsNumber()
+  postId: number;
+
+  @IsNumber()
+  userId: number;
+}
