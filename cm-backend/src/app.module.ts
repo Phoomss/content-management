48b5d0ee7e0b74306cd2d7c9b18085d7bc9ddc07
Phoomss <narongsak5az@gmail.com>
2026-02-02 17:17:52 +0700

    setting for development with docker

--- cm-backend/src/app.module.ts
+++ cm-backend/src/app.module.ts
@@ -5,22 +5,29 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'nest_cm',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: true, // dev only 
+      synchronize: true,
+      retryAttempts: 10,
+      retryDelay: 3000,
     }),
-    UsersModule, 
-    PostsModule, 
-    CommentsModule
+    UsersModule,
+    PostsModule,
+    CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
